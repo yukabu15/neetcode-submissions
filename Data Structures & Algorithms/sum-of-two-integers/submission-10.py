721cd4fc8b9d49e9905e4a49aa0144427937bd99
Yuki Inoue <147043231+yukabu15@users.noreply.github.com>
2026-04-28 23:49:57 +0900

    Add: sum-of-two-integers - submission-10

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-10.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        ans = 0
+        c = 0
+        max_int = 0x7FFFFFFF
+        mask = 0xFFFFFFFF
+        for i in range(32):
+            a_bit = (a >> i) & 1
+            b_bit = (b >> i) & 1
+            xor = a_bit ^ b_bit
+            s = xor ^ c
+            c = (a_bit & b_bit) | (a_bit & c) | (b_bit & c)
+            if s:
+                ans |= (1 << i)
+
+        if ans > max_int:
+            ans = ~(ans ^ mask)
+        
+        return ans
